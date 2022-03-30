79b3915322c5376b7635ceddf2afb3cf1e870c50
19521352 <80874241+dattran2803@users.noreply.github.com>
2022-03-30 22:33:09 +0700

    soft delete

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -5,9 +5,20 @@ class MeController {
   //[GET] /me/stored/courses
   storedCourses(req, res, next) {
     Course.find({})
-      .then(courses => res.render('me/stored-courses', {
+      .then((courses) => res.render('me/stored-courses', {
         courses: mutipleMongooseToObject(courses),
-      }))
+      }),
+      )
+      .catch(next);
+  }
+
+  //[GET] /me/stored/courses
+  trashCourses(req, res, next) {
+    Course.findDeleted({})
+      .then((courses) => res.render('me/trash-courses', {
+        courses: mutipleMongooseToObject(courses),
+      }),
+      )
       .catch(next);
   }
 }
