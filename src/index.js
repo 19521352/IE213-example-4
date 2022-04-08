7ba2306c83314a3239712959d45b4b68d04cb7d7
19521352 <80874241+dattran2803@users.noreply.github.com>
2022-04-09 00:58:30 +0700

    Sort middleware

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const methodOverride = require('method-override');
 const { engine } = require('express-handlebars');
 
+const SortMiddleware = require('./app/middleware/SortMiddleware')
+
 const route = require('./routes');
 const db = require('./config/db');
 
@@ -13,11 +15,7 @@ db.connect();
 const app = express();
 const port = 3000;
 
-// HTTP logger
-// app.use(morgan('combined'))
-
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(methodOverride('_method'));
 
 app.use(
   express.urlencoded({
@@ -26,6 +24,14 @@ app.use(
 );
 app.use(express.json());
 
+app.use(methodOverride('_method'));
+
+// Custom middlewares
+app.use(SortMiddleware)
+
+// HTTP logger
+// app.use(morgan('combined'))
+
 // Template engine
 app.engine(
   'hbs',
@@ -33,6 +39,26 @@ app.engine(
     extname: '.hbs',
     helpers: {
       sum: (a, b) => a + b,
+      sortable: (field, sort) => {
+        const sortType = field === sort.column ? sort.type : 'default'
+
+        const icons = {
+          default: 'oi oi-elevator',
+          asc: 'oi oi-sort-ascending',
+          desc: 'oi oi-sort-descending'
+        }
+        const types = {
+          default: 'desc',
+          asc: 'desc',
+          desc: 'asc'
+        }
+
+        const icon = icons[sortType]
+        const type = types[sortType]
+        return `<a href="?_sort&column=${field}&type=${type}">
+            <span class="${icon}"></span>
+          </a>`
+      }
     },
   }),
 );
